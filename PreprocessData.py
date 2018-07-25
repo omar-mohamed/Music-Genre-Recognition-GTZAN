e104d4e3e599534638ab11f5f71ea6f910da9390
Omar Mohamed <omar.mohamed.latif@gmail.com>
2018-07-25 16:29:38 +0200

    -added more features to data

--- PreprocessData.py
+++ PreprocessData.py
@@ -23,12 +23,31 @@ import librosa.display
 #http://opihi.cs.uvic.ca/sound/genres.tar.gz
 #GTZAN genre collection.
 
+# rand=np.random.randint(11, size=(1000, 41,1400))
+
+# def normalize_2(x):
+#     xvar, xavg = x.var(axis=0), x.mean(axis=0)
+#     print(xavg)
+#     x = (x - xavg)
+#     xvar_0=xvar==0
+#     if(xvar_0!=False):
+#         xvar[xvar_0]=1
+#     x=x/xvar
+#     print("After normalization:")
+#     xmin, xmax = x.min(), x.max()
+#     print(xmin)
+#     print(xmax)
+#
+#     return x
+#
+# normalize_2(np.array([0,0,0,0,0,0,0,0,-20]))
+
 y,sr=librosa.load('./genres/blues/blues.00001.au')
 
-librosa.display.waveplot(y,sr)
+librosa.display.waveplot(y, sr)
 
 D=librosa.stft(y)
-
+#D_short = librosa.stft(y, hop_length=6400)
 log_power=librosa.amplitude_to_db(librosa.magphase(D)[0], ref=np.max)
 
 librosa.display.specshow(log_power,x_axis='time',y_axis='log')
@@ -40,6 +59,9 @@ print(mfcc)
 
 def read_data(directory='./genres'):
     all_data_mfcc=np.zeros((1000,20,1400),dtype=float)
+    all_data_spectrogram=np.zeros((1000,20,1400),dtype=float)
+    all_data_beats=np.zeros((1000,1,1400),dtype=float)
+
     all_labels=np.zeros(1000)
     label_index=0
     image_index=0
@@ -49,32 +71,43 @@ def read_data(directory='./genres'):
                 for file in files:
                     y, sr = librosa.load(directory+'/'+dir+'/'+file)
                     mfcc = librosa.feature.mfcc(y=y, sr=sr)
-
+                    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=20, fmax=1400)
+                    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+                    all_data_spectrogram[image_index,:,0:S.shape[1]]=S
                     all_data_mfcc[image_index,:,0:mfcc.shape[1]]=mfcc
+                    all_data_beats[image_index,:,0:beats.shape[0]]=beats
+                    all_data_beats[image_index, :, beats.shape[0]+1] = tempo
                     all_labels[image_index]=label_index
                     image_index=image_index+1
             label_index=label_index+1
-    return all_data_mfcc,all_labels
+    all_data=np.concatenate((all_data_mfcc,all_data_spectrogram),axis=1)
+    all_data=np.concatenate((all_data,all_data_beats),axis=1)
 
-all_mfcc,all_labels=read_data()
+    return all_data,all_labels
+
+all_data,all_labels=read_data()
 
 def normalize(x):
     xvar, xavg = x.var(axis=0), x.mean(axis=0)
     print(xavg)
     x = (x - xavg)
-    xvar[xvar==0]=1
+    xvar_0=xvar==0
+    if(isinstance(xvar_0,bool)==False):
+        xvar[xvar_0]=1
     x=x/xvar
     print("After normalization:")
     xmin, xmax = x.min(), x.max()
-    print(x)
+    print(xmin)
+    print(xmax)
+
     return x
 
 
-print(all_mfcc.shape)
+print(all_data.shape)
 
-all_mfcc=normalize(all_mfcc)
+all_data=normalize(all_data)
 
-print(all_mfcc.shape)
+print(all_data.shape)
 
 
 def randomize(dataset, labels):
@@ -83,12 +116,12 @@ def randomize(dataset, labels):
     shuffled_labels = labels[permutation]
     return shuffled_dataset, shuffled_labels
 
-all_mfcc,all_labels=randomize(all_mfcc,all_labels)
+all_data,all_labels=randomize(all_data,all_labels)
 
 
 def split_data(dataset,labels,num_classes=10,test_images_for_class=25):
     test_counter=np.zeros(num_classes)
-    test_set=np.zeros((num_classes*test_images_for_class,20,1400),dtype=float)
+    test_set=np.zeros((num_classes*test_images_for_class,dataset.shape[1],dataset.shape[2]),dtype=float)
     test_labels=np.zeros(num_classes*test_images_for_class)
     deleted_index=[]
     test_index=0
@@ -106,10 +139,10 @@ def split_data(dataset,labels,num_classes=10,test_images_for_class=25):
 
     return dataset,labels,test_set,test_labels
 
-train_set,train_labels,test_set,test_labels=split_data(all_mfcc,all_labels)
+train_set,train_labels,test_set,test_labels=split_data(all_data,all_labels)
 
 
-pickle_file = 'dataset_normalized.pickle'
+pickle_file = 'dataset_normalized_beats.pickle'
 
 try:
     f = open(pickle_file, 'wb')
