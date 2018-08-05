0c533a221de129c432d5a523f9d00a6794b79991
Omar Mohamed <omar.mohamed.latif@gmail.com>
2018-08-05 15:35:45 +0200

    -added comments to GMM

--- Train_GMM.py
+++ Train_GMM.py
@@ -1,22 +1,13 @@
 from __future__ import print_function
-import os
 import numpy as np
-import tensorflow as tf
-from sklearn import svm
 from six.moves import cPickle as pickle
-import scipy.io
-from matplotlib import pyplot as plt
-import random
-
-
-# from tensorflow.python.client import device_lib
-# print (device_lib.list_local_devices())
-
-
-##################load data#####################
 from sklearn.decomposition import PCA
 from sklearn.mixture import GMM
-from sklearn.svm import LinearSVC, SVC
+
+##################load data#####################
+
+print("Loading data")
+
 
 all_data = pickle.load(open('dataset_standarized_all_10.pickle', 'rb'))
 train_data = all_data['train_dataset']
@@ -27,58 +18,73 @@ test_labels = all_data['test_labels']
 
 del all_data
 
-
+# in case you want to select a portion of the features
 # start_index=0
 # end_index=87
 # train_data=train_data[:,start_index:end_index,:]
 # test_data=test_data[:,start_index:end_index,:]
 
 
-num_channels = 1  # grayscale
-image_width = train_data.shape[1]
-image_height = train_data.shape[2]
+input_width = train_data.shape[1]
+input_height = train_data.shape[2]
 
+
+# vectorize the data
 def reformat(dataset):
     dataset = dataset.reshape(
-        (-1, image_width*image_height)).astype(np.float32)
+        (-1, input_width * input_height)).astype(np.float32)
     return dataset
 
 
 train_data = reformat(train_data)
 test_data = reformat(test_data)
 
+print("Shape of training set after vectorization:")
+print(train_data.shape)
 
-print('train_data shape is : %s' % (train_data.shape,))
-print('test_data shape is : %s' % (test_data.shape,))
+print("Shape of test set after vectorization:")
+print(test_data.shape)
 
+################## PCA #####################
 
-test_size = test_data.shape[0]
-train_size = train_data.shape[0]
+print("Running PCA")
 
-
-
-pca=PCA(copy=True, iterated_power='auto', n_components=900, random_state=None,
-  svd_solver='auto', tol=0.0, whiten=False)
+# run pca to reduce vector size to 900
+pca = PCA(copy=True, iterated_power='auto', n_components=900, random_state=None,
+          svd_solver='auto', tol=0.0, whiten=False)
 
 pca.fit(train_data)
 
-train_data=pca.transform(train_data)
+train_data = pca.transform(train_data)
 
-test_data=pca.transform(test_data)
+test_data = pca.transform(test_data)
 
-n_classes = 10
+print("Shape of training set after pca:")
+print(train_data.shape)
 
-# Try GMMs using different types of covariances.
-classifiers = dict((covar_type, GMM(n_components=n_classes,
-                    covariance_type=covar_type, init_params='wc', n_iter=5))
-                   for covar_type in ['full'])
+print("Shape of test set after pca:")
+print(test_data.shape)
+
+################## GMM #####################
 
 
+# computes accuracy given the predictions and real labels
 def accuracy(predictions, labels):
     batch_size = predictions.shape[0]
     sum = np.sum(predictions == labels)
     acc = (100.0 * sum) / batch_size
     return acc
+
+
+n_classes = 10  # 10 genre classes
+
+# Try GMMs using different types of covariances. I'm only letting 'full' as it performs better but can add different types to try
+classifiers = dict((covar_type, GMM(n_components=n_classes,
+                                    covariance_type=covar_type, init_params='wc', n_iter=5))
+                   for covar_type in ['full'])
+
+
+print("Training GMM")
 
 for index, (name, classifier) in enumerate(classifiers.items()):
     # Since we have class labels for the training data, we can
@@ -86,11 +92,8 @@ for index, (name, classifier) in enumerate(classifiers.items()):
     classifier.means_ = np.array([train_data[train_labels == i].mean(axis=0)
                                   for i in range(n_classes)])
 
-
     # Train the other parameters using the EM algorithm.
     classifier.fit(train_data)
-
-
 
     train_predictions = classifier.predict(train_data)
 
